bcdcbca158aecc46d7f8af0edf5f02f7dad81690
ANGJustinl <ANGJustinl@users.noreply.github.com>
2024-02-10 15:52:22 +0000

    chore: update config file

--- static/scripts/config.js
+++ static/scripts/config.js
@@ -1,5 +1,16 @@
 export default {
   items: [
-  
+    "meme/FUCK U U U.jpg",
+    "meme/hotfix",
+    "meme/superpx.jpg",
+    "meme/别让爱成为等待.jpg",
+    "meme/好对.jpg",
+    "meme/差别对待.jpg",
+    "meme/有南通.jpg",
+    "meme/烧姑妈.jpg",
+    "meme/烧老登.jpg",
+    "meme/老登.jpg",
+    "meme/装什么嫩.jpg",
+    "meme/黑幕.jpg"
   ]
 }
